0e3abc858611e8aa74f6d2d957f549800aac89a4
Blair White <white.blair1@gmail.com>
2016-02-12 18:48:51 -0800

    Include all files

---
+++ week-4/my_solution1.rb
@@ -0,0 +1,16 @@
+
+# I worked on this challenge with Chunyu Ou.
+# Your Solution Below
+number = ARGV[0]
+def factorial(number)
+  fact_ = 1
+  while number > 0
+    fact_ = fact_ * number
+    number = number - 1
+  end
+  fact_
+end
+
+
+
+  # Your code goes here
