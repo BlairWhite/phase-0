c7545dc5587225aa7bea2499901306cde0ae05db
Blair White <white.blair1@gmail.com>
2016-03-10 16:36:11 -0800

    Add fibonacci w reflection

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -1,8 +1,9 @@
-# I worked on this challenge [by myself, with: ].
-# This challenge took me [#] hours.
+# I worked on this challenge by myself.
+# This challenge took me 2 hours.
 
 # Pseudocode
-#generate Fibonacci numbers in the sequence until the most recent generated number is
+#generate Fibonacci numbers in the sequence until the most recent generated number is larger or equal to original number
+#if number is equal, it is fib approved
 
 # Initial Solution
 
@@ -14,6 +15,7 @@
 # return true if num == fib_array[-1]
 # return false
 # end
+
 # Refactored Solution
 def is_fibonacci?(num)
   fib_array = [0, 1]
@@ -23,3 +25,8 @@ def is_fibonacci?(num)
 return true if num == fib_array[-1]
 return false
 end
+
+#Reflection
+#This challenge helped me to brush up on Ruby while loops, arrays, and .push.
+#I am very comfortable with Ruby. JS however...
+#I am going to do all these Ruby review challenges and go back and do bonus exercises.
